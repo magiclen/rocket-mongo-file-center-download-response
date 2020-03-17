eacdf7e5b5db1c64dbf21943fc776a8e2ef0bbaa
Magic Len <len@magiclen.org>
2020-03-17 10:33:52 +0800

    update dependencies

--- examples/cached_download_temporary_image.rs
+++ examples/cached_download_temporary_image.rs
@@ -18,7 +18,7 @@ use rocket::request::State;
 
 use validators::short_crypt_url_component::ShortCryptUrlComponent;
 
-const URI: &str = "mongodb://localhost:27017";
+const URI: &str = "mongodb://localhost:27017/test_rocket_mongo_file_center_download_response";
 
 #[get("/<id_token>")]
 fn download(
@@ -33,9 +33,7 @@ fn download(
 }
 
 fn main() {
-    let database = "test_rocket_mongo_file_center_download_response";
-
-    let file_center = FileCenter::new(URI, database).unwrap();
+    let file_center = FileCenter::new(URI).unwrap();
 
     let path = Path::join(Path::new("examples"), Path::join(Path::new("images"), "image(貓).jpg"));
 
